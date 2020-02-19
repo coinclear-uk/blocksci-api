c19dd141de8ba223c9e020fc792ac7d553f2f7b5
coinclear-uk <aj.johnstone.coinclear@gmail.com>
2020-02-19 16:09:04 +0000

    fixed get transactions for an address

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-#import blocksci
+import itertools
+import blocksci
 from bottle import auth_basic, json_dumps, debug, error, response, route, install, run, HTTPError, HTTPResponse
 
 from json_responses import *
@@ -24,62 +25,89 @@ def view_404(code):
 @route('/address/search/<address_string:re:[13][a-km-zA-HJ-NP-Z1-9]{25,34}>/')
 #@auth_basic(check)
 def address_search(address_string):
-	data = {'transactions':{}}
-	'''
-	chain = blocksci.Blockchain(settings.BLOCKSCI_LOCATION)
-	address = chain.address_from_string(address_string)
-	data['type'] = address.type
-	data['balance'] = address.balance()
-	data['transactions']['first'] = address.first_tx
-	data['transactions']['last'] = address.last_tx
-	data['transactions']['total'] = {}
-	data['transactions']['total']['in'] = address.in_txes_count()
-	ata['transactions']['total']['out'] = address.out_txes_count()
-	data['equivalent'] = []
-	for equivalent_addresses in address.equiv():
-		address_data = {'type':equivalent_addresses.type}
-		if equivalent_addresses.type not in ['NonStandardAddress','OpReturn']
-			address_data['address'] = equivalent_addresses.address_string
-	'''
-	return json_success(data)
+    data = {'transactions':{}}
+    chain = blocksci.Blockchain(settings.BLOCKSCI_CONFIG)
+    address = chain.address_from_string(address_string)
+    data['type'] = str(address.type)
+    data['balance'] = address.balance()
+    data['transactions']['first'] = str(address.first_tx.hash)
+    data['transactions']['total'] = {}
+    data['transactions']['total']['in'] = address.in_txes_count()
+    data['transactions']['total']['out'] = address.out_txes_count()
+    #data['equivalent'] = []
+    #for address in address.equiv().addresses:
+    #    if address.type not in [blocksci.address_type.nonstandard, blocksci.address_type.nulldata]:
+    #        address_data = {'type': address.type, 'address': address.address_string}
+    #        data['equivalent'].append(address_data)
+    return json_success(data)
 
+
+#@auth_basic(check)
 @route('/address/<address:re:[13][a-km-zA-HJ-NP-Z1-9]{25,34}>/transactions/from/')
-#@auth_basic(check)
-def address_transactions_from(address,page):
-	return json_success(address_transactions_from_pagnated(address_search,1))
+def address_transactions_from(address):
+	return address_transactions_pagnated(True, address, 1)
 
+
+#@auth_basic(check)
 @route('/address/<address:re:[13][a-km-zA-HJ-NP-Z1-9]{25,34}>/transactions/from/<page:int>/')
-#@auth_basic(check)
 def address_transactions_from(address,page):
-	return json_success(address_transactions_from_pagnated(address_search,page))
+	return address_transactions_pagnated(True, address, page)
 
-def address_transactions_from_pagnated(address_string, page):
-	'''
-	chain = blocksci.Blockchain(settings.BLOCKSCI_LOCATION)
-	address = chain.address_from_string(address_string)
-	data = []
-	for transaction in address.in_txes():
-		t = {
-			'hash': transactions.hash,
-			'block': transaction.block_height,
-			'time_stamp': transaction.block_time.strftime("%H:%m %d/%m/%Y"),
-			'fee': transaction.fee,
-		}
-		if t.change_output is not None:
-			t['change_address'] = t.change_output.address
-		for input in transaction.inputs:
-			input_data = {'type':input.type}
-			if input.type not in ['NonStandardAddress','OpReturn']:
-				input_data['address'] = input.address_string
-			t['inputs'] = input_data
-		for output in transaction.outputs:
-			output_data = {'type':output.type}
-			if output.type not in ['NonStandardAddress','OpReturn']:
-				output_data['address'] = output.address_string
-			t['outputs'] = output_data
-		data.append(t)
-	'''
-	pass
+
+#@auth_basic(check)
+@route('/address/<address:re:[13][a-km-zA-HJ-NP-Z1-9]{25,34}>/transactions/to/')
+def address_transactions_to(address):
+	return address_transactions_pagnated(False, address, 1)
+
+
+#@auth_basic(check)
+@route('/address/<address:re:[13][a-km-zA-HJ-NP-Z1-9]{25,34}>/transactions/to/<page:int>/')
+def address_transactions_to(address,page):
+	return address_transactions_pagnated(False, address, page)
+
+
+def address_transactions_pagnated(is_from, address_string, page):
+    chain = blocksci.Blockchain(settings.BLOCKSCI_CONFIG)
+    try:
+        address = chain.address_from_string(address_string)
+    except AttributeError:
+        return json_not_found()
+    if is_from:
+    	transactions_as_list = list(address.out_txes)
+    else:
+        transactions_as_list = list(address.in_txes)
+    start = (page - 1) * settings.RESULTS_PER_PAGE
+    end = page * settings.RESULTS_PER_PAGE
+    data = {
+        'start': start,
+        'end': end,
+        'total': len(transactions_as_list),
+        'result': []
+    }
+    for transaction in transactions_as_list[start:end]:
+        t = {
+            'hash': str(transaction.hash),
+            'block': transaction.block_height,
+            'timestamp': transaction.block_time.strftime("%H:%m %d/%m/%Y"),
+            'fee': transaction.fee,
+            'inputs': [],
+            'outputs': [],
+        }
+        #if transaction.change_output is not None:
+        #    t['change_address'] = str(transaction.change_output.address)
+        for tx_input in transaction.inputs:
+            input_data = {'address':tx_input.address.address_string,'value':in_bitcoins(tx_input.value)}
+            t['inputs'].append(input_data)
+        for tx_output in transaction.outputs:
+            output_data = {'address':tx_output.address.address_string,'value':in_bitcoins(tx_output.value),'spent':tx_output.is_spent}
+            t['outputs'].append(output_data)
+        data['result'].append(t)
+    return json_success(data)
+
+
+def in_bitcoins(satoshis):
+    return float(satoshis)/100000000
+
 
 debug(settings.DEBUG)
 
---
+++ blocksci/Blockchain-Known-Pools/README.md
@@ -0,0 +1,11 @@
+Blockchain Pools
+======================
+
+Bitcoin Mining Known Pools Tracking Tags for https://blockchain.info/pools & https://www.blockchain.com/pools
+
+Contributions welcome.
+
+Guidelines
+======================
+- Provide only 1 mining pool by PR
+- The website provided must be available 
