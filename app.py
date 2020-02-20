04acf06a3e548151286ca6e0b729f35edb117ae1
coinclear-uk <aj.johnstone.coinclear@gmail.com>
2020-02-20 11:44:23 +0000

    updated details endpoint to include more relevant info

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import itertools
 import blocksci
+from datetime import datetime, timedelta
 from bottle import auth_basic, json_dumps, debug, error, response, route, install, run, HTTPError, HTTPResponse
 
 from json_responses import *
@@ -22,18 +23,48 @@ def view_403(code):
 def view_404(code):
 	return json_not_found()
 
-@route('/address/search/<address_string:re:[13][a-km-zA-HJ-NP-Z1-9]{25,34}>/')
 #@auth_basic(check)
+@route('/address/<address_string:re:[13][a-km-zA-HJ-NP-Z1-9]{25,34}>/details/')
 def address_search(address_string):
+    now = datetime.now()
     data = {'transactions':{}}
     chain = blocksci.Blockchain(settings.BLOCKSCI_CONFIG)
     address = chain.address_from_string(address_string)
+    txes = list(address.txes)
+    total_in = 0
+    total_out = 0
+    total_day = 0
+    total_month = 0
+    total_year = 0
+    for tx in txes:
+        '''for tx_input in tx.inputs:
+            try:
+                if tx_input.address == address:
+                    total_out += 1
+            except AttributeError:
+                pass
+        for tx_output in tx.outputs:
+            try:
+                if tx_output.address == address:
+                    total_in += 1
+            except AttributeError:
+                pass'''
+        if tx.block_time > (now - timedelta(days=1)):
+            total_day += 1
+        if tx.block_time > (now - timedelta(days=30)):
+            total_month += 1
+        if tx.block_time > (now - timedelta(days=365)):
+            total_year += 1
     data['type'] = str(address.type)
-    data['balance'] = address.balance()
-    data['transactions']['first'] = str(address.first_tx.hash)
+    data['balance'] = in_bitcoins(address.balance())
+    data['transactions']['first'] = {'hash':str(txes[0].block.hash),'block':txes[0].block.height,'date':str(txes[0].block_time)}
+    data['transactions']['last'] = {'hash':str(txes[-1].block.hash),'block':txes[-1].block.height,'date':str(txes[-1].block_time)}
     data['transactions']['total'] = {}
-    data['transactions']['total']['in'] = address.in_txes_count()
-    data['transactions']['total']['out'] = address.out_txes_count()
+    data['transactions']['total']['in'] = address.out_txes_count()
+    data['transactions']['total']['out'] = address.in_txes_count()
+    data['transactions']['total']['day'] = total_day
+    data['transactions']['total']['month'] = total_month
+    data['transactions']['total']['year'] = total_year
     #data['equivalent'] = []
     #for address in address.equiv().addresses:
     #    if address.type not in [blocksci.address_type.nonstandard, blocksci.address_type.nulldata]:
@@ -94,6 +125,4 @@ def in_bitcoins(satoshis):
     return float(satoshis)/100000000
 
 
-debug(settings.DEBUG)
-
-run(server=settings.SERVER, host=settings.HOST, port=settings.PORT, reloader=settings.RELOAD)
+run(server=settings.SERVER, host=settings.HOST, port=settings.PORT, reloader=settings.RELOAD, debug=settings.DEBUG)
